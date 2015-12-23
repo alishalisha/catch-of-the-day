54897cf4c7ea09a2c8853210b7bca065d2fc622f
alishalisha <alisha.ramos@voxmedia.com>
2015-12-23 17:46:02 -0500

    up to lesson 3

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+/*
+  Store Picker component
+  <StorePicker/>
+*/
+
+var StorePicker = React.createClass({
+
+  render : function() {
+    return (
+      <p>hello</p>
+    )
+  }
+});
+
+
+ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+
